2ad0e58dd5e4ac470f8a61230ce99e7b31f7d9b2
Danail Donkov <danail.donkov5@gmail.com>
2026-03-10 20:47:12 +0200

    Update row.cpp

--- Group E/2018/row.cpp
+++ Group E/2018/row.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    string s;
+    int k = 0, count = 0;
+    cin >> s;
+    cin >> k;
+
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        int zeros = 0;
+        for (int j = i; j < n; j++)
+        {
+            if (s[j] == '0')
+                zeros++;
+            if (zeros == k) count++;
+            if (zeros > k) break;
+        }
+    }
+    cout << count << endl;
+}
