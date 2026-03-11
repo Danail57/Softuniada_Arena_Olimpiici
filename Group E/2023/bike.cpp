77df29f0e9ab10bc1c0cf46c95bf5ffd7f288668
Danail Donkov <danail.donkov5@gmail.com>
2026-03-11 20:24:04 +0200

    Update bike.cpp

--- Group E/2023/bike.cpp
+++ Group E/2023/bike.cpp
@@ -1,47 +1,90 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
-long long go_ahead_to_station(int n, int p, int staions[3])
+
+long long int diff(long long n, long long p, long long stations[3])
 {
-	long long 
+
+   // long long dist, dist2, dist3;
+   long long dist = min(abs(p - stations[0]), n - abs(p - stations[0]));
+   long long dist2 = min(abs(p - stations[1]), n - abs(p - stations[1]));
+   long long dist3 = min(abs(p - stations[2]), n - abs(p - stations[2]));
+
+   long long distances[] = { dist, dist2, dist3 };
+   long long nearest_dist_to_station = n;
+
+   for (auto d : distances)
+   {
+      if (d < nearest_dist_to_station)
+      {
+         nearest_dist_to_station = d;
+      }
+   }
+
+   return abs(nearest_dist_to_station);
 }
-
-
-long long int diff(int n, int p, int stations[3])
-{
-	long long dist, dist2, dist3;
-	long long distances[] = { dist, dist2, dist3 };
-	long long nearest_dist_to_station = 0;
-
-	for (int i = 0; i < 3; i++)
-	{
-		dist = abs(p - stations[i]);
-		dist2 = abs(p - stations[i + 1]);
-		dist3 = abs(p - stations[i + 2]);
-
-		for (auto d: distances)
-		{
-			if (dist < nearest_dist_to_station || dist == nearest_dist_to_station)
-			{
-				nearest_dist_to_station = dist;
-			}
-		}
-	}
-	return abs(nearest_dist_to_station);
-}
-
 
 int main()
 {
-	long long n, p;
-	cin >> n >> p;
+   long long n, p;
+   cin >> n >> p;
 
-	int stations[3];
+   long long stations[3];
 
-	for (int i = 0; i < 3; i++)
-	{
-		cin >> stations[i];
-	}
-	cout << diff();
+   for (int i = 0; i < 3; i++)
+   {
+      cin >> stations[i];
+   }
+   cout << diff(n, p, stations) <<endl;
 }
+
+
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+
+// long long go_ahead_to_station(int n, int p, int staions[3])
+// {
+// 	long long 
+// }
+
+
+// long long int diff(int n, int p, int stations[3])
+// {
+// 	long long dist, dist2, dist3;
+// 	long long distances[] = { dist, dist2, dist3 };
+// 	long long nearest_dist_to_station = 0;
+
+// 	for (int i = 0; i < 3; i++)
+// 	{
+// 		dist = abs(p - stations[i]);
+// 		dist2 = abs(p - stations[i + 1]);
+// 		dist3 = abs(p - stations[i + 2]);
+
+// 		for (auto d: distances)
+// 		{
+// 			if (dist < nearest_dist_to_station || dist == nearest_dist_to_station)
+// 			{
+// 				nearest_dist_to_station = dist;
+// 			}
+// 		}
+// 	}
+// 	return abs(nearest_dist_to_station);
+// }
+
+
+// int main()
+// {
+// 	long long n, p;
+// 	cin >> n >> p;
+
+// 	int stations[3];
+
+// 	for (int i = 0; i < 3; i++)
+// 	{
+// 		cin >> stations[i];
+// 	}
+// 	cout << diff();
+// }
