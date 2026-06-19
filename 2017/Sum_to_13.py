1b3b70b886dbcbbf2a49cc1c08cf49f1a25f3d3f
Danail Donkov <danail.donkov5@gmail.com>
2026-06-19 23:13:55 +0300

    Update Sum_to_13.py

--- 2017/Sum_to_13.py
+++ 2017/Sum_to_13.py
@@ -1,1 +1,8 @@
-
+a, b, c = map(int, input().split())
+if ((a + b + c == 13) or
+    (a + b - c == 13) or
+    (a - b + c == 13) or
+    (-a + b + c == 13)):
+    print("Yes")
+else:
+    print("No")
